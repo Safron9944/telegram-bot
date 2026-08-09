4059464ebae219e61682a7d8388477a8c09d72e2
Safron9944 <safron.9944z@gmail.com>
2026-08-09 21:28:17 +0300

    Remove menu subtitles and reorder home cards

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-import "/static/js/app.js?v=20260809-title-only-01";
+import "/static/js/app.js?v=20260809-menu-titles-01";
 
 const confirmedFinishButtons = new WeakSet();
 const pendingFinishButtons = new WeakSet();
