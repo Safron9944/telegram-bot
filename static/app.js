0e1051a13cd20857f07c71ff5edba089b811572d
Safron9944 <safron.9944z@gmail.com>
2026-08-10 12:23:43 +0300

    Simplify attestation sections (#28)
    Remove the attestation summary cards and show the question range under each of the four parts.

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-import "/static/js/app.js?v=20260810-ios-fullscreen-02";
+import "/static/js/app.js?v=20260810-attestation-ranges-01";
 
 const confirmedFinishButtons = new WeakSet();
 const pendingFinishButtons = new WeakSet();
