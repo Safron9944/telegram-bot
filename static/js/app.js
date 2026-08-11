c7eefbed7764694a10f4e235a6642021e8db26ae
Safron9944 <safron.9944z@gmail.com>
2026-08-11 23:24:33 +0300

    allow deleting attestation sections: static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -62,7 +62,7 @@ import {
   renderAdminUserDetail,
   renderAdminUsers,
   runQuestionSearch,
-} from "./screens/admin.js?v=20260811-attestation-management-01";
+} from "./screens/admin.js?v=20260811-delete-all-attestation-01";
 import { renderCurrentView } from "./screens/session.js?v=20260810-result-navigation-01";
 
 const PROTOTYPE_SCREENS = new Set([
