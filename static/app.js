c86cc714e2a50e70e68238b0aec0a180e3f2c956
Safron9944 <safron.9944z@gmail.com>
2026-08-11 23:24:31 +0300

    allow deleting attestation sections: static/app.js

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-import "/static/js/app.js?v=20260811-attestation-start-retry-01";
+import "/static/js/app.js?v=20260811-delete-all-attestation-01";
 
 const confirmedFinishButtons = new WeakSet();
 const pendingFinishButtons = new WeakSet();
