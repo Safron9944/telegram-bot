9362c3a70c16db3b94d4b1cf8ae1d35d57c1c760
Safron9944 <safron.9944z@gmail.com>
2026-09-22 09:10:45 +0300

    Bump static cache for OK level selection

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-import "/static/js/app.js?v=20260922-inline-ok-levels-12";
+import "/static/js/app.js?v=20260922-ok-level-select-13";
 
 const confirmedFinishButtons = new WeakSet();
 const pendingFinishButtons = new WeakSet();
